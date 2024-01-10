5af9a32e34274070a1029b86a53293f49053ac8b
Nggohel <gohelnidhi15898@gmail.com>
2024-01-10 09:53:32 +0530

    add

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -5,18 +5,37 @@ import LoginCarousel from "../components/Carousels/LoginCarousel";
 import "../styles/LoginPage.css";
 const LoginPage = () => {
   const [open, setOpen] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleLogin = () => {
+    setIsLogin(true);
+  };
+
   return (
     <>
-      <Modal open={open} onClose={() => setOpen(false)}>
+      <Modal
+        open={open}
+        center
+        onClose={() => setOpen(false)}
+        classNames={{
+          modal: "customModal",
+        }}
+      >
         <div style={{ display: "flex" }}>
           <LoginCarousel />
           <div className="login-container">
-            <label className="name-label">Name</label>
-            <input
-              type="text"
-              className="input-name"
-              placeholder="Enter Your Name"
-            ></input>
+            {isLogin ? (
+              ""
+            ) : (
+              <>
+                <label className="name-label">Name</label>
+                <input
+                  type="text"
+                  className="input-name"
+                  placeholder="Enter Your Name"
+                ></input>
+              </>
+            )}
             <label className="name-label">Email</label>
             <input
               type="email"
@@ -24,7 +43,19 @@ const LoginPage = () => {
               placeholder="Enter Your Email"
             ></input>
 
-            <button className="login-btn">Get Login</button>
+            <button className="login-btn">Get start</button>
+            {isLogin ? (
+              ""
+            ) : (
+              <>
+                <p className="already-acc-para">
+                  Already account ?
+                  <span onClick={handleLogin} className="already-acc">
+                    Login Here
+                  </span>
+                </p>
+              </>
+            )}
           </div>
         </div>
       </Modal>
